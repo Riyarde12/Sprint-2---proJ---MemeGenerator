655b753499ebf85f5aeef6b6b021125a632e9071
Riyarde12 <yarden.rihsoni03@gmail.com>
2022-02-16 17:00:02 +0200

    render meme with txt on canvas

--- js/controller-meme.js
+++ js/controller-meme.js
@@ -4,18 +4,61 @@ var gCanvas;
 var gCtx;
 
 function init() {
-    gCanvas = document.querySelector('canvas');
+    gCanvas = document.getElementById('my-canvas');
     gCtx = gCanvas.getContext('2d');
 
     renderMeme();
 }
 
 function renderMeme() {
+    const meme = getMeme();
+    drawImg(meme.selectedImgId);
+}
+
+function drawImg(imgId) {
+    const meme = getMeme();
     var img = new Image();
-    img.src = 'imgs/1.jpg';
+    console.log(img.src);
     img.onload = () => {
         gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height);
+        drawText(30, 30, meme.lines[0].txt);
     };
+    img.src = `${getImgForDisplay(imgId).url}`;
+}
+
+function drawText(x, y, text) {
+    // gCtx.font = '48px serif';
+    // gCtx.fillText(text, x, y);
+
+    gCtx.lineWidth = 1;
+    gCtx.strokeStyle = 'brown';
+    gCtx.fillStyle = 'black';
+    gCtx.font = '20px Arial';
+    gCtx.fillText(text, x, y);
+    gCtx.strokeText(text, x, y);
+}
+
+function draw(ev) {
+    const offsetX = ev.offsetX;
+    const offsetY = ev.offsetY;
+
+    gShape.pos = getEvPos(ev);
+    console.log(offsetX);
+    // const currShape = getShape();
+    // currShape = { offsetX, offsetY };
+    // console.log(offsetX,offsetY)
+    // const { offsetX, offsetY } = ev
+    switch (gUserShape) {
+        case 'triangle':
+            drawTriangle(offsetX, offsetY);
+            break;
+        case 'rect':
+            drawRect(offsetX, offsetY);
+            break;
+        case 'arc':
+            drawArc(offsetX, offsetY);
+            break;
+    }
 }
 
 function onOpenEditor() {
--- js/service-meme.js
+++ js/service-meme.js
@@ -1,1 +1,30 @@
 'use strict';
+
+var gImgs = [
+    { id: 1, url: 'imgs/1.jpg', keywords: ['funny', 'cat'] }
+];
+
+var gMeme = {
+    selectedImgId: 1,
+    selectedLineIdx: 0,
+    lines: [
+        {
+            txt: 'I sometimes eat Falafel',
+            size: 20,
+            align: 'left',
+            color: 'red'
+        }
+    ]
+};
+
+function getImg() {
+    return gImgs;
+}
+
+function getMeme() {
+    return gMeme;
+}
+
+function getImgForDisplay(imgId) {
+    return gImgs.find(img => imgId === img.id);
+}
