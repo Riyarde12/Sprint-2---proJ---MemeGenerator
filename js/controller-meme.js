98a4e50b2e6f043c1badc3a688cbed9c9dbf1489
Riyarde12 <yarden.rihsoni03@gmail.com>
2022-02-16 19:56:40 +0200

    render gallery

--- js/controller-meme.js
+++ js/controller-meme.js
@@ -6,7 +6,8 @@ var gCtx;
 function init() {
     gCanvas = document.getElementById('my-canvas');
     gCtx = gCanvas.getContext('2d');
-
+    createImgs();
+    renderGallery();
     renderMeme();
 }
 
@@ -18,12 +19,12 @@ function renderMeme() {
 function drawImg(imgId) {
     const meme = getMeme();
     var img = new Image();
-    console.log(img.src);
     img.onload = () => {
         gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height);
         drawText(30, 30, meme.lines[0].txt);
     };
-    img.src = `${getImgForDisplay(imgId).url}`;
+    img.src = `${getImgById(imgId).url}`;
+    console.log(img.src);
 }
 
 function drawText(x, y, text) {
@@ -38,31 +39,51 @@ function drawText(x, y, text) {
     gCtx.strokeText(text, x, y);
 }
 
-function draw(ev) {
-    const offsetX = ev.offsetX;
-    const offsetY = ev.offsetY;
+// function draw(ev) {
+//     const offsetX = ev.offsetX;
+//     const offsetY = ev.offsetY;
 
-    gShape.pos = getEvPos(ev);
-    console.log(offsetX);
-    // const currShape = getShape();
-    // currShape = { offsetX, offsetY };
-    // console.log(offsetX,offsetY)
-    // const { offsetX, offsetY } = ev
-    switch (gUserShape) {
-        case 'triangle':
-            drawTriangle(offsetX, offsetY);
-            break;
-        case 'rect':
-            drawRect(offsetX, offsetY);
-            break;
-        case 'arc':
-            drawArc(offsetX, offsetY);
-            break;
-    }
+//     gShape.pos = getEvPos(ev);
+//     console.log(offsetX);
+//     // const currShape = getShape();
+//     // currShape = { offsetX, offsetY };
+//     // console.log(offsetX,offsetY)
+//     // const { offsetX, offsetY } = ev
+//     switch (gUserShape) {
+//         case 'triangle':
+//             drawTriangle(offsetX, offsetY);
+//             break;
+//         case 'rect':
+//             drawRect(offsetX, offsetY);
+//             break;
+//         case 'arc':
+//             drawArc(offsetX, offsetY);
+//             break;
+//     }
+// }
+
+function renderGallery() {
+
+    const imgs = getImg();
+    var strHtml = imgs.map(img => {
+        return `<img src="imgs/${img.id}.jpg" alt="" onclick="onOpenEditor(this)">`;
+    }).join('');
+    console.log('strHtml', strHtml);
+    var elGrid = document.querySelector('.grid');
+    elGrid.innerHTML = strHtml;
 }
 
-function onOpenEditor() {
-    console.log('hey!');
+function onSelectedImg(imgUrl) {
+    getImgByUrl(imgUrl);
+}
+
+function onOpenEditor(elImg) {
+    var imgUrl = elImg.src;
+
+    onSelectedImg(imgUrl);
+
+    console.log('img', imgUrl);
     document.querySelector('.my-modal').classList.add = ('open');
     document.querySelector('.my-modal').style.left = '50%';
 }
+
--- js/service-meme.js
+++ js/service-meme.js
@@ -1,8 +1,25 @@
 'use strict';
 
-var gImgs = [
-    { id: 1, url: 'imgs/1.jpg', keywords: ['funny', 'cat'] }
+const memesSentences = [
+    'I never eat falafel',
+    'DOMS DOMS EVERYWHERE',
+    'Stop Using i in for loops',
+    'Armed in knowledge',
+    'Js error "Unexpected String"',
+    'One does not simply write js',
+    'I`m a simple man i see vanilla JS, i click like!',
+    'JS, HTML,CSS?? Even my momma can do that',
+    'May the force be with you',
+    'I know JS',
+    'JS Where everything is made up and the rules dont matter',
+    'Not sure if im good at programming or good at googling',
+    'But if we could',
+    'JS what is this?',
+    'Write hello world , add to cv 7 years experienced',
 ];
+
+var gIdx = 1;
+var gImgs = [];
 
 var gMeme = {
     selectedImgId: 1,
@@ -17,6 +34,26 @@ var gMeme = {
     ]
 };
 
+function createMeme() {
+
+}
+
+function createImgs() {
+    var imgs = [];
+    for (var i = 0; i < 9; i++) {
+        imgs.push(createImg());
+    }
+    gImgs = imgs;
+    console.log(gImgs);
+}
+
+function createImg() {
+    return {
+        id: gIdx,
+        url: `imgs/${gIdx++}.jpg`,
+    };
+}
+
 function getImg() {
     return gImgs;
 }
@@ -25,6 +62,13 @@ function getMeme() {
     return gMeme;
 }
 
-function getImgForDisplay(imgId) {
+function getImgByUrl(imgUrl) {
+    var currImg = gImgs.find(img => imgUrl === img.url);
+    console.log(currImg);
+}
+
+function getImgById(imgId) {
     return gImgs.find(img => imgId === img.id);
 }
+
+
